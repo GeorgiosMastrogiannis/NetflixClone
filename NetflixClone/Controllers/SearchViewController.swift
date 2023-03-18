563b76d473eda2bcd740ed20482f9d0cc777e73e
George <George>
2023-03-18 18:14:31 +0100

    Add MainTabBarViewController

---
+++ NetflixClone/Controllers/SearchViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SearchViewController.swift
+//  NetflixClone
+//
+//  Created by Georgios Mastrogiannis on 18.03.2023.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+}
---
+++ NetflixClone/Controllers/UpcomingViewController.swift
@@ -0,0 +1,16 @@
+//
+//  UpcomingViewController.swift
+//  NetflixClone
+//
+//  Created by Georgios Mastrogiannis on 18.03.2023.
+//
+
+import UIKit
+
+class UpcomingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+}
